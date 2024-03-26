7d23815d6a82fa4c4ba17282c3ce02c2bc59ba0d
The2Innkeeper <139631318+The2Innkeeper@users.noreply.github.com>
2024-03-26 02:50:58 +0000

    Deploying to gh-pages from @ The2Innkeeper/3DWebBallistics@2e06bb6f4a55458e0579d43ce84fe0838580e0ed 🚀

--- index.05ba4a2a.js
+++
@@ -1,2 +0,0 @@
-const e=new THREE.OrbitControls(camera,renderer.domElement);camera.position.set(0,5,30),e.update();
-//# sourceMappingURL=index.05ba4a2a.js.map
---
+++ index.e874b071.js
@@ -0,0 +1,6 @@
+// Add orbit controls
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
+camera.position.set(0, 5, 30);
+controls.update();
+
+//# sourceMappingURL=index.e874b071.js.map
